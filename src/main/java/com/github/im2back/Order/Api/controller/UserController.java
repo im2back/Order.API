e4eb55d1dbeecdcc08c24a35c3f91114068fbeff
Jefferson Richards <jefferson.richards@outlook.com>
2023-08-16 11:48:07 -0300

    Service layer, component registration and adjustment in controller layer

--- src/main/java/com/github/im2back/Order/Api/controller/UserController.java
+++ src/main/java/com/github/im2back/Order/Api/controller/UserController.java
@@ -1,19 +1,34 @@
 package com.github.im2back.Order.Api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.im2back.Order.Api.model.user.User;
+import com.github.im2back.Order.Api.services.UserService;
 
 @RestController
 @RequestMapping(value = "users")
 public class UserController {
-
+	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	ResponseEntity<User> findAll() {
-		User user = new User(1L, "teste", "teste", "teste", "teste");
+	ResponseEntity<List<User>> findAll() {
+		List<User> user = service.findAll();
+		return ResponseEntity.ok().body(user);
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 
---
+++ src/main/java/com/github/im2back/Order/Api/services/UserService.java
@@ -0,0 +1,26 @@
+package com.github.im2back.Order.Api.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.im2back.Order.Api.model.user.User;
+import com.github.im2back.Order.Api.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = repository.findById(id);
+		return user.get();
+	}
+}
