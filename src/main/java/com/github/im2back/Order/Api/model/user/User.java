bcf3ff0b021e7c1904039eef8b1ede247917301b
Jefferson Richards <jefferson.richards@outlook.com>
2023-08-16 11:16:04 -0300

    H2 database,test profile, JPA

--- src/main/java/com/github/im2back/Order/Api/model/user/User.java
+++ src/main/java/com/github/im2back/Order/Api/model/user/User.java
@@ -2,6 +2,11 @@ package com.github.im2back.Order.Api.model.user;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,14 +18,15 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
+@Entity
+@Table(name= "tb_user")
 public class User implements Serializable{
-	
-/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
-private Long id;
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
 	private String name;
 	private String email;
