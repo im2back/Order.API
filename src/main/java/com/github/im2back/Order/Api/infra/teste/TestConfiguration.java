2fc809970c8710980abdf7aadd61ae2dd0ab993c
Jefferson Richards <jefferson.richards@outlook.com>
2023-08-17 15:12:56 -0300

    Bug fix in test configuration

--- src/main/java/com/github/im2back/Order/Api/infra/teste/TestConfiguration.java
+++ src/main/java/com/github/im2back/Order/Api/infra/teste/TestConfiguration.java
@@ -38,7 +38,7 @@ public class TestConfiguration implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123457");
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		
 		
